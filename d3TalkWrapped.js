df2ea72ae1a2157bf7a4271943ea244829720cae
justincoh <justincoh@gmail.com>
2015-06-16 10:09:55 -0400

    updated axes switching in wrapped

--- d3TalkWrapped.js
+++ d3TalkWrapped.js
@@ -2,16 +2,13 @@ var width = 800,
     height = 600;
 
 var xMeasure = 'frustration';
+var yMeasure = 'caffeineInSystem';
 //Create the SVG 
 var svg = d3.select('.chart').append('svg')
     .attr('width', width)
     .attr('height', height)
     .style('overflow', 'visible')
 
-
-var yDomain = d3.extent(data, function(d) {
-    return d.skills;
-});
 
 //dot colors
 var colorDomain = d3.extent(data, function(d) {
@@ -23,27 +20,10 @@ var color = d3.scale.linear()
 
 //radius sizing
 var radius = d3.scale.linear()
-    .domain(d3.extent(data,function(d){return d.caffeineInSystem;}))
+    .domain(d3.extent(data, function(d) {
+        return d.caffeineInSystem;
+    }))
     .range(['5px', '50px'])
-
-//Build Y Axis
-var y = d3.scale.linear()
-    .domain([yDomain[0], 100])
-    .range([height, 0]); //backwards because of svg coordinates
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient('left');
-
-//Draw Y Axis
-svg.append('g')
-    .call(yAxis)
-    .append('text')
-    .attr('transform', 'rotate(-90)')
-    .style('text-anchor', 'end')
-    .attr('x', -height / 2.4)
-    .attr('y', -35)
-    .text('Skills')
 
 //Add Dots
 var dots = svg.selectAll('dots')
@@ -56,10 +36,16 @@ var dots = svg.selectAll('dots')
     .style('opacity', '.9')
 
 var update = function() {
-    xMeasure = xMeasure === 'time' ? 'frustration' : 'time';
-    //Touch on max/min, use extent
+    xMeasure = xMeasure === 'time' ? 'skills' : 'time';
+    yMeasure = yMeasure === 'skills'? 'frustration':'skills';
+    // yMeasure = 'skills';
+    
     var xDomain = d3.extent(data, function(d) {
         return d[xMeasure];
+    });
+
+    var yDomain = d3.extent(data, function(d) {
+        return d[yMeasure];
     });
 
     //Build x Axis
@@ -72,9 +58,9 @@ var update = function() {
         .orient('bottom');
 
     //Draw x axis
-    d3.select('.x-axis').remove()
+    d3.select('.x-axis').remove();
     var xAxisPath = svg.append('g')
-        .attr('class','x-axis')
+        .attr('class', 'x-axis')
         .call(xAxis)
         .attr('transform', 'translate(0,' + height + ')') //moves down to bottom of svg
         .append('text')
@@ -84,18 +70,41 @@ var update = function() {
         .text(xMeasure)
 
 
+    //Build Y Axis
+    var y = d3.scale.linear()
+        .domain(yDomain)
+        .range([height, 0]); //backwards because of svg coordinates
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient('left');
+
+    //Draw Y Axis
+    d3.select('.y-axis').remove();
+    svg.append('g')
+        .attr('class','y-axis')
+        .call(yAxis)
+        .append('text')
+        .attr('transform', 'rotate(-90)')
+        .style('text-anchor', 'end')
+        .attr('x', -height / 2.4)
+        .attr('y', -35)
+        .text(yMeasure)
+
+
+
     dots
         .transition().duration(1000)
         .attr('cx', function(d) {
             return x(d[xMeasure])
         })
         .attr('cy', function(d) {
-            return y(d.skills)
+            return y(d[yMeasure])
         })
         .attr('r', function(d) {
             return radius(d.caffeineInSystem);
         })
-        
+
 }
 
 update();
